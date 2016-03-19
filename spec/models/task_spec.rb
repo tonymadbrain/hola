81fe3672b8a472c9b8ea991c199a5eda1a9ff805
Antony Ryabov <mb7222@yandex.ru>
2016-03-20 01:18:47 +0300

    Added model User

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,9 @@
 require File.expand_path '../../spec_helper.rb', __FILE__
 
 RSpec.describe Task, type: :model do
+  it { should respond_to(:name) }
+  it { should respond_to(:description) }
+  it { should respond_to(:state) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:description) }
   it { should validate_length_of(:name).is_at_most(25) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require File.expand_path '../../spec_helper.rb', __FILE__
 
 RSpec.describe User, type: :model do
   it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation)}
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   # it { should validate_presence_of(:name) }
   # it { should validate_presence_of(:email) }
   # it { should validate_presence_of(:password) }
