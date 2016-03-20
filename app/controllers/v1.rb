f42fab883b68b271d13de3b5f7f17af393fa8e7e
Antony Ryabov <mb7222@yandex.ru>
2016-03-20 18:06:11 +0300

    Added new endpoint /users

--- app/controllers/v1.rb
+++ app/controllers/v1.rb
@@ -6,6 +6,7 @@ namespace '/api/v1' do
       "name":"MyTask",
       "description":"some useful description",
       "state":"new",
+      "user_id":1,
       "created_at":"2016-03-11T12:28:01.380Z",
       "updated_at":"2016-03-11T12:28:01.380Z"
     }
@@ -30,7 +31,6 @@ namespace '/api/v1' do
     if @tasks = Task.limit(limit).offset(offset)
       tasks_count = Task.count
       headers['X-Total-Count'] = "#{tasks_count}"
-      offsets = {}
       link = ""
       url = request.url.split('?').first
       offset_first = 0
@@ -107,6 +107,7 @@ namespace '/api/v1' do
       "name":"MyTask",
       "description":"some useful description",
       "state":"new",
+      "user_id":1,
       "created_at":"2016-03-11T20:05:44.192Z",
       "updated_at":"2016-03-11T20:05:44.192Z"
     }
@@ -164,6 +165,7 @@ namespace '/api/v1' do
     status 400
     status 404
   end
+
   delete '/tasks/:id' do
     @task = Task.find_by_id(params[:id])
     return status 404 if @task.nil?
@@ -171,6 +173,63 @@ namespace '/api/v1' do
       status 202
     else
       status 400
+    end
+  end
+
+  documentation "Respond with list of existed users" do
+    param :limit, "limit of users for response, default is 20, max is 100"
+    param :offset, "offset for users, default is 0"
+    response "", {
+      "id":1,
+      "name":"MyTask",
+      "e-mail":"mail@example.org",
+      "created_at":"2016-03-11T12:28:01.380Z",
+      "updated_at":"2016-03-11T12:28:01.380Z"
+    }
+    status 200,"If all work properly"
+    status 400
+    status 404
+  end
+  get '/users' do
+    params[:limit] ||= 20
+    params[:offset] ||= 0
+    limit = params[:limit].to_i
+    offset = params[:offset].to_i
+
+    if limit < 0 or limit > 100
+      return json_error "Invalid limit", 400
+    end
+
+    if offset < 0
+      return json_error "Invalid offset", 400
+    end
+
+    if @users = User.limit(limit).offset(offset)
+      users_count = User.count
+      headers['X-Total-Count'] = "#{users_count}"
+      link = ""
+      url = request.url.split('?').first
+      offset_first = 0
+      link += "<#{url}?offset=#{offset_first}&limit=#{limit}>; rel=\"first\","
+      offset_last = users_count - limit
+      link += "<#{url}?offset=#{offset_last}&limit=#{limit}>; rel=\"last\","
+      offset_next = offset + limit
+      link += "<#{url}?offset=#{offset_next}&limit=#{limit}>; rel=\"next\","
+      if offset != 0
+        if offset >= limit
+          offset_prev = offset - limit
+          link += "<#{url}?offset=#{offset_prev}&limit=#{limit}>; rel=\"prev\","
+        end
+        if offset < limit
+          offset_prev = 0
+          limit = offset
+          link += "<#{url}?offset=#{offset_prev}&limit=#{limit}>; rel=\"prev\","
+        end
+      end
+      headers['Link'] = link
+      json @users
+    else
+      json_error
     end
   end
 end
--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -1,28 +1,28 @@
-# require File.expand_path '../../../spec_helper.rb', __FILE__
-#
-# describe 'Users API' do
-#   describe 'GET /users' do
-#     context 'when have users' do
-#       before do
-#         User.create(email:'user1@hola.api', password: 'user1@hola.api')
-#         User.create(email:'user2@hola.api', password: 'user2@hola.api')
-#         get '/api/v1/users'
-#       end
-#
-#       it 'provide header X-Total-Count' do
-#         expect(last_response.headers['X-Total-Count']).to eq("#{User.count}")
-#       end
-#
-#       it 'respond with 200 ok' do
-#         expect(last_response).to be_ok
-#       end
-#
-#       it 'respond with right objects' do
-#         data = JSON::parse(last_response.body)
-#         expect(data.size).to eq(2)
-#         expect(data[0]['email']).to eq('user1@hola.api')
-#         expect(data[1]['email']).to eq('user2@hola.api')
-#       end
-#     end
-#   end
-# end
+require File.expand_path '../../../spec_helper.rb', __FILE__
+
+describe 'Users API' do
+  describe 'GET /users' do
+    context 'when have users' do
+      before do
+        User.create(email:'user1@hola.api', password: 'user1@hola.api')
+        User.create(email:'user2@hola.api', password: 'user2@hola.api')
+        get '/api/v1/users'
+      end
+
+      it 'provide header X-Total-Count' do
+        expect(last_response.headers['X-Total-Count']).to eq("#{User.count}")
+      end
+
+      it 'respond with 200 ok' do
+        expect(last_response).to be_ok
+      end
+
+      it 'respond with right objects' do
+        data = JSON::parse(last_response.body)
+        expect(data.size).to eq(2)
+        expect(data[0]['email']).to eq('user1@hola.api')
+        expect(data[1]['email']).to eq('user2@hola.api')
+      end
+    end
+  end
+end
